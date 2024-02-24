55a15e4c1f9ce8ad56d5910cdb08a216604e4554
inescontilde <astody2@gmail.com>
2024-02-24 23:21:55 +0100

    Ahora funciona el login

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::resource("productos", MiauController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('main');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
